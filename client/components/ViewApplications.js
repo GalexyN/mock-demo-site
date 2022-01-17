4697bef30fe5be48e24eddce7d44b57f4c095af4
GalexyN <alexander.nguyen@informed.iq>
2022-01-17 13:35:48 -0800

    added bottom spacing between button and text field

--- client/components/ViewApplications.js
+++ client/components/ViewApplications.js
@@ -2,10 +2,27 @@ import * as React from 'react';
 import Typography from '@mui/material/Typography';
 import Title from './Title';
 import ViewApplicationModal from './ViewApplicationModal';
+import Box from '@mui/material/Box';
+import Grid from '@mui/material/Grid';
+import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
 
 function preventDefault(event) {
     event.preventDefault();
 }
+
+const style = {
+    // position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    width: '25%',
+    height: '70%',
+    bgcolor: '#fff',
+    border: '2px solid #2b6777',
+    boxShadow: 24,
+    p: 4,
+};
 
 export default function ViewApplications() {
     return (
@@ -18,7 +35,15 @@ export default function ViewApplications() {
                 utilizes VerifyIQ
             </Typography>
             <div>
-                <ViewApplicationModal />
+                {/* <ViewApplicationModal /> */}
+                <Grid container align="center" rowSpacing="10px">
+                    <Grid item xs={12}>
+                        <TextField id="standard-basic" label="Application ID" variant="standard" />
+                    </Grid>
+                    <Grid item xs={12}>
+                        <Button variant="contained">Find App</Button>
+                    </Grid>
+                </Grid>
             </div>
         </React.Fragment>
     );
